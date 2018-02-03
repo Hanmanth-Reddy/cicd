6a25c1edf6169af18e7382bf2192dd8de4b80201
PARTNERS\hpatukur <hpatukur@cisco.com>
2018-02-03 15:51:47 +0530

    done

---
+++ jenkins/files/jenkins-jobs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+jenkins_home=/var/lib/jenkins
+
+for i in $*
+do
+		if [ ! -d "${jenkins_home}/jobs/${i}" ]
+		then
+				java -jar ${jenkins_home}/jenkins-cli.jar  -s http://localhost:8080/  create-job ${i}  <  /tmp/${i}_config.xml
+		else
+				java -jar ${jenkins_home}/jenkins-cli.jar  -s http://localhost:8080/  update-job ${i}  <  /tmp/${i}_config.xml
+		fi
+done
