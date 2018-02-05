c3e748d3e0c246e575fe95dd41ac22566d7818a7
root <root@localhost.localdomain>
2018-02-05 06:16:26 -0500

    done

--- jenkins/files/jenkins-jobs.sh
+++ jenkins/files/jenkins-jobs.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+set -x 
 
 jenkins_home=/var/lib/jenkins
 
-for i in $*
+for i in $@
 do
-		if [ ! -d "${jenkins_home}/jobs/${i}" ]
+		if [ ! -d ${jenkins_home}/jobs/${i} ]
 		then
 				java -jar ${jenkins_home}/jenkins-cli.jar  -s http://localhost:8080/  create-job ${i}  <  /tmp/${i}_config.xml
 		else
 				java -jar ${jenkins_home}/jenkins-cli.jar  -s http://localhost:8080/  update-job ${i}  <  /tmp/${i}_config.xml
 		fi
 done
+
+set +x
